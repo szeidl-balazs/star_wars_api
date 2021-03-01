50fd9b7301a6a39aafc43ce758d70f53e5625e1a
TamasKrisztian <kortanaz@gmail.com>
2021-03-01 20:23:27 +0100

    added a table

--- frontend/apiwars/src/components/Table.js
+++ frontend/apiwars/src/components/Table.js
@@ -1,10 +1,33 @@
 function Table({ planets }) {
   return (
-    <div>
-      {planets.map((planet) => (
-        <h2 key={planet.name}>{planet.name}</h2>
-      ))}
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Terrain</th>
+          <th>Surface Water Percentage</th>
+          <th>Population</th>
+          <th>Residents</th>
+        </tr>
+      </thead>
+      <tbody>
+        {planets.map((planet) => (
+          <tr key={planet.name}>
+            <td>{planet.name}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>
+              <button>{planet.residents.length} residents</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
